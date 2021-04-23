17ebacb97a43e0aecc1b86bae2f05a421ab0eb19
Zachary <rick.jsventures.com>
2021-04-23 00:10:58 -0400

    reading data from websocket

---
+++ bot.py
@@ -0,0 +1,29 @@
+# https://www.youtube.com/watch?v=GdlFhF6gjKo&t=404s&ab_channel=PartTimeLarry
+
+import websocket, json, pprint
+
+SOCKET = "wss://stream.binance.com:9443/ws/dogeusdt@kline_1m"
+
+closes = []
+in_position = False
+
+def on_open(ws):
+    print('opened connection')
+
+def on_close(ws):
+    print('closed connection')
+
+def on_message(ws, message):
+    global closes, in_position
+    
+    print('received message')
+    json_message = json.loads(message)
+    pprint.pprint(json_message)
+
+    candle = json_message['k']
+
+    # is_candle_closed = candle['x']
+    # close = candle['c']
+
+ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
+ws.run_forever()
